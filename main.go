514a2e25a93165f31e2d3dd1c610774007c02adf
Marcin Kaszynski <marcink@codility.com>
2017-05-31 00:16:31 +0200

    Reload config on HUP.

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 // TODO: add TLS to listener and admin
-// TODO: add TLS to uplink
+// TODO: add TLS to uplink (including reloads)
 // TODO: better names for resp New*er
 // TODO: proper logging
 // TODO: guard config access with a mutex
@@ -10,17 +10,24 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
+const CONFIG_FILE = "config.json"
+
 func main() {
-	config, err := parseConfig("config.json")
+	config, err := loadConfig(CONFIG_FILE)
 	if err != nil {
 		panic(err)
 	}
-	proxy := RedisProxy{config}
+	proxy := RedisProxy{config: config}
 	proxy.run()
 }
 
@@ -34,7 +41,7 @@ type RedisProxyConfig struct {
 	AdminOn      string `json:"admin_on"`
 }
 
-func parseConfig(fname string) (*RedisProxyConfig, error) {
+func loadConfig(fname string) (*RedisProxyConfig, error) {
 	configJson, err := ioutil.ReadFile(fname)
 	if err != nil {
 		return nil, err
@@ -43,10 +50,21 @@ func parseConfig(fname string) (*RedisProxyConfig, error) {
 	return &config, json.Unmarshal(configJson, &config)
 }
 
+func (config *RedisProxyConfig) canReload(newConfig *RedisProxyConfig) error {
+	if config.ListenOn != newConfig.ListenOn {
+		return errors.New("New config must have the same listen_on address as the old one.")
+	}
+	if config.AdminOn != newConfig.AdminOn {
+		return errors.New("New config must have the same admin_on address as the old one.")
+	}
+	return nil
+}
+
 ////////////////////////////////////////
 // RedisProxy
 
 type RedisProxy struct {
+	mu     sync.Mutex
 	config *RedisProxyConfig
 }
 
@@ -58,6 +76,8 @@ func (proxy *RedisProxy) run() {
 
 	fmt.Println("Listening on", proxy.config.ListenOn)
 
+	go proxy.watchSignals()
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -67,18 +87,64 @@ func (proxy *RedisProxy) run() {
 	}
 }
 
+func (proxy *RedisProxy) getConfig() *RedisProxyConfig {
+	proxy.mu.Lock()
+	defer proxy.mu.Unlock()
+
+	return proxy.config
+}
+
+func (proxy *RedisProxy) setConfig(config *RedisProxyConfig) {
+	proxy.mu.Lock()
+	defer proxy.mu.Unlock()
+
+	proxy.config = config
+}
+
+func (proxy *RedisProxy) reloadConfig() {
+	newConfig, err := loadConfig(CONFIG_FILE)
+	if err != nil {
+		fmt.Printf("Got an error while loading %s: %s.  Keeping old config.", CONFIG_FILE, err)
+		return
+	}
+
+	if err := newConfig.canReload(proxy.getConfig()); err != nil {
+		fmt.Printf("Can not reload into new config: %s.  Keeping old config.", err)
+		return
+	}
+	proxy.setConfig(newConfig)
+}
+
+func (proxy *RedisProxy) watchSignals() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP)
+
+	for {
+		s := <-c
+		fmt.Printf("Got signal: %v, reloading config\n", s)
+		proxy.reloadConfig()
+	}
+}
+
 func (proxy *RedisProxy) handleClient(cliConn net.Conn) {
+	// TODO: catch and log panics
 	defer cliConn.Close()
 	cliReader := NewReader(bufio.NewReader(cliConn))
 	cliWriter := bufio.NewWriter(cliConn)
 
-	// TODO: catch and log panics
-	fmt.Println("Dialing", proxy.config.UplinkAddr)
-	uplinkConn, err := net.Dial("tcp", proxy.config.UplinkAddr)
+	config := proxy.getConfig()
+
+	fmt.Println("Dialing", config.UplinkAddr)
+	uplinkConn, err := net.Dial("tcp", config.UplinkAddr)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Dial error: %v\n", err)
+		return
 	}
-	defer uplinkConn.Close()
+	defer func() {
+		// TODO: does this actually close the right connection
+		// after reloads?
+		uplinkConn.Close()
+	}()
 	uplinkReader := NewReader(bufio.NewReader(uplinkConn))
 	uplinkWriter := bufio.NewWriter(uplinkConn)
 
@@ -88,6 +154,21 @@ func (proxy *RedisProxy) handleClient(cliConn net.Conn) {
 			fmt.Printf("Read error: %v\n", err)
 			return
 		}
+
+		currConfig := proxy.getConfig()
+		if config != currConfig {
+			config = currConfig
+			fmt.Println("Redialing", config.UplinkAddr)
+			uplinkConn.Close()
+			uplinkConn, err = net.Dial("tcp", config.UplinkAddr)
+			if err != nil {
+				fmt.Printf("Dial error: %v\n", err)
+				return
+			}
+			uplinkReader = NewReader(bufio.NewReader(uplinkConn))
+			uplinkWriter = bufio.NewWriter(uplinkConn)
+		}
+
 		uplinkWriter.Write(req)
 		uplinkWriter.Flush()
 
